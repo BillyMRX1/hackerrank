8a2de287ba5da12166267e19296c739a7b29f1d3
BillyMRX1 <brilianadeputra@gmail.com>
2024-06-04 09:50:12 +0700

    Initial Commit

---
+++ algorithm/compareWithTriplets/Solution.java
@@ -0,0 +1,64 @@
+package hackerrank.algorithm.compareWithTriplets;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> result = Arrays.asList(0, 0);
+        int position = 0;
+
+        while (position < 3) {
+            if (a.get(position) > b.get(position)) {
+                result.set(0, result.get(0) + 1);
+            } else if (a.get(position) < b.get(position)) {
+                result.set(1, result.get(1) + 1);
+            }
+            position++;
+        }
+
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ algorithm/solveMeFirst/solveMeFirst.java
@@ -0,0 +1,12 @@
+package hackerrank.algorithm.solveMeFirst;
+
+import java.util.*;
+
+public class solveMeFirst {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
