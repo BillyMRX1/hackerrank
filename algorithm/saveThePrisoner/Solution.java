77be8477473ae4a6110fc3eca3158a91dc906001
BillyMRX1 <brilianadeputra@gmail.com>
2024-06-11 15:21:24 +0700

    Save The Prisoner

--- algorithm/saveThePrisoner/Solution.java
+++ algorithm/saveThePrisoner/Solution.java
@@ -38,33 +38,31 @@ class Result {
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//        int t = Integer.parseInt(bufferedReader.readLine().trim());
-//
-//        IntStream.range(0, t).forEach(tItr -> {
-//            try {
-//                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-//
-//                int n = Integer.parseInt(firstMultipleInput[0]);
-//
-//                int m = Integer.parseInt(firstMultipleInput[1]);
-//
-//                int s = Integer.parseInt(firstMultipleInput[2]);
-//
-//                int result = Result.saveThePrisoner(n, m, s);
-//
-//                bufferedWriter.write(String.valueOf(result));
-//                bufferedWriter.newLine();
-//            } catch (IOException ex) {
-//                throw new RuntimeException(ex);
-//            }
-//        });
-//
-//        bufferedReader.close();
-//        bufferedWriter.close();
-        //System.out.println(Result.saveThePrisoner(7, 19, 2));
-        System.out.println(Result.saveThePrisoner(3, 7, 3));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.saveThePrisoner(n, m, s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
