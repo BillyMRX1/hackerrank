7af846721d0818e388cd82fc08efaea1cda185e1
BillyMRX1 <brilianadeputra@gmail.com>
2024-06-12 15:34:44 +0700

    Update Find Digits

---
+++ algorithm/findDigits/Solution.java
@@ -0,0 +1,73 @@
+package hackerrank.algorithm.findDigits;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * t = number of test case
+ * n = digit
+ * Time Complexity: 𝑂(t * n)
+ * Space Complexity: 𝑂(n)
+ */
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        // Write your code here
+        String numberAsString = String.valueOf(n);
+        char[] digitsArray = numberAsString.toCharArray();
+        int totalDivisor = 0;
+
+        for (char c : digitsArray) {
+            int divisor = Integer.parseInt(String.valueOf(c));
+            if (divisor != 0 && n % divisor == 0) {
+                totalDivisor++;
+            }
+        }
+
+        return totalDivisor;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
